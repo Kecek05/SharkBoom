673af73dede4e2cfa1142706ba1a1f0762d4ea4e
KeceK <jfk.abrao@hotmail.com>
2025-04-16 07:55:22 -0300

    Start direction of the drag component

---
+++ Assets/Scripts/Player/PlayerDetectFacingDirection.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerDetectFacingDirection : MonoBehaviour
+{
+    //Based on the direction of the other player, the default direction is right is true when the enemy is on the right side
+    private bool isDefaultDirectionRight = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
