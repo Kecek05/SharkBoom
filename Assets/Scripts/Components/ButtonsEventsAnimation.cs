b1c203e2eebf3c33a6e49bfb103cf710d9f67688
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-06-02 12:10:04 -0300

    Saving files

--- Assets/Scripts/Components/ButtonsEventsAnimation.cs
+++ Assets/Scripts/Components/ButtonsEventsAnimation.cs
@@ -3,22 +3,20 @@ using UnityEngine;
 
 public class ButtonsEventsAnimation : MonoBehaviour
 {
-    [SerializeField] private MMFeedbacks clickFeedback;
-    [SerializeField] private MMFeedbacks holdFeedback;
-    [SerializeField] private MMFeedbacks releaseFeedback;
+    
 
     public void OnClickFeedback()
     {
-        clickFeedback?.PlayFeedbacks();
+
     }
 
     public void OnHoldFeedback()
     {
-        holdFeedback?.PlayFeedbacks();
+
     }
 
     public void OnReleaseFeedback()
     {
-        releaseFeedback?.PlayFeedbacks();
+
     }
 }
