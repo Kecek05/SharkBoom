121ad323536ac513f8f32a208c6172611a50fef2
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-06-12 10:54:36 -0300

    Ajusting ui animator

--- Assets/Scripts/UI/UiAnimator.cs
+++ Assets/Scripts/UI/UiAnimator.cs
@@ -10,36 +10,21 @@ public class UiAnimator : MonoBehaviour
 
     [BetterHeader("Settings")]
     [SerializeField] private float fadeTime;
-    [SerializeField] private float distanceForMovePanel;
+    [SerializeField] private float startPanelScale;
+    [SerializeField] private float endPanelScale;
     [SerializeField] private Ease fadeInAnimation;
-    [SerializeField] private Ease fadeOutAnimation;
 
 
     private void OnEnable()
     {
         PanelFadeIn();
-        Debug.Log("FadeIn");
-    }
-
-    private void OnDisable()
-    {
-        PanelFadeOut();
-        Debug.Log("FadeOut");
     }
 
     private void PanelFadeIn()
     {
         canvasGroup.alpha = 0;
-        rectTransform.transform.localPosition = new Vector3(0f, -distanceForMovePanel, 0f);
-        rectTransform.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(fadeInAnimation);
+        transform.localScale = new Vector3(startPanelScale, startPanelScale, startPanelScale);
+        transform.DOScale(new Vector3(endPanelScale, endPanelScale, endPanelScale), fadeTime).SetEase(fadeInAnimation);
         canvasGroup.DOFade(1, fadeTime);
-    }
-
-    private void PanelFadeOut()
-    {
-        canvasGroup.alpha = 1;
-        rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
-        rectTransform.DOAnchorPos(new Vector2(0f, -distanceForMovePanel), fadeTime, false).SetEase(fadeOutAnimation);
-        canvasGroup.DOFade(0, fadeTime);
     }
 }
