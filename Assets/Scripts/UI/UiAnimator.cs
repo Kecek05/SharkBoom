6c255141d63644f217aae95a421632fab471fd00
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-06-15 01:10:14 -0300

    Correcting ui animator component

--- Assets/Scripts/UI/UiAnimator.cs
+++ Assets/Scripts/UI/UiAnimator.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using DG.Tweening;
 using Sortify;
+using UnityEngine.Events;
 
 public class UiAnimator : MonoBehaviour
 {
@@ -12,19 +13,31 @@ public class UiAnimator : MonoBehaviour
     [SerializeField] private float fadeTime;
     [SerializeField] private float startPanelScale;
     [SerializeField] private float endPanelScale;
+    [SerializeField] private Vector3 movePosition;
     [SerializeField] private Ease fadeInAnimation;
 
+    public UnityEvent OnUiAnimation;
 
     private void OnEnable()
     {
-        PanelFadeIn();
+        OnUiAnimation?.Invoke();
     }
 
-    private void PanelFadeIn()
+    public void ChangeTransparency()
     {
         canvasGroup.alpha = 0;
+        canvasGroup.DOFade(1, fadeTime);
+    }
+
+    public void ChangeScale()
+    {
         transform.localScale = new Vector3(startPanelScale, startPanelScale, startPanelScale);
         transform.DOScale(new Vector3(endPanelScale, endPanelScale, endPanelScale), fadeTime).SetEase(fadeInAnimation);
-        canvasGroup.DOFade(1, fadeTime);
+    }
+
+    public void ChangeMove()
+    {
+        rectTransform.transform.localPosition = movePosition;
+        rectTransform.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(fadeInAnimation);
     }
 }
