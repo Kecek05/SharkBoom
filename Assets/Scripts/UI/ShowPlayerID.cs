0dc16875864ced8baac340524fd8eeee47b18476
KeceK <jfk.abrao@hotmail.com>
2025-06-18 17:06:09 -0300

    Fixed bug with Server Build

--- Assets/Scripts/UI/ShowPlayerID.cs
+++ Assets/Scripts/UI/ShowPlayerID.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class ShowPlayerID : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI playerID;
-
+#if UNITY_ANDROID
     private void Start()
     {
         playerID.text = $"ID: {AuthenticationWrapper.GetPlayerID()}";
-    }
+    } 
+#endif
 }
