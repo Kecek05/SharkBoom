721e2cfab27925da8356536f080c428c8ddbaea1
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-06-26 22:12:22 -0300

    Correcting game tutorial scene

--- Assets/Scripts/GameTutorialScene/GameTutorialHandler.cs
+++ Assets/Scripts/GameTutorialScene/GameTutorialHandler.cs
@@ -1,7 +1,25 @@
+using System;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class GameTutorialHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject loadingPanel;
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += HandleOnVideoPlayerPrepared;
+    }
+
+    private void HandleOnVideoPlayerPrepared(VideoPlayer source)
+    {
+        Debug.Log("Teste");
+        loadingPanel.SetActive(false);
+        source.Play();
+    }
+
     public void FinishedTutorial()
     {
         Loader.LoadNoLoadingScreen(Loader.Scene.MainMenu);
